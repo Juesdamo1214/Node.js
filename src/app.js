b973fd2a41ddf5e30a737c33d60a9cec4b205fbd
Juesdamo1214 <68707535+Juesdamo1214@users.noreply.github.com>
2023-05-09 11:48:30 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import projectsRoutes from './routes/projects.routes.js'
+import taskRoutes from "./routes/task.routes.js";
+
+const app = express()
+
+app.use(express.json())
+
+app.use(projectsRoutes)
+app.use(taskRoutes)
+
+export default app
